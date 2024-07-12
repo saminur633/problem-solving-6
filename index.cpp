b88550ab2d022a7cefe166f99a4ab0dba67ae36c
saminur633 <rahmansaminur51@gmail.com>
2024-07-12 22:47:46 +0600

    added some problem

--- index.cpp
+++ index.cpp
@@ -302,5 +302,34 @@ int main() {
     return 0;
 }
 
+// problem 16
+#include <iostream>
+using namespace std;
+
+int main() {
+    float X, Y;
+    
+    // Read the input numbers
+    cin >> X >> Y;
+    
+    // Check the location of the point
+    if (X == 0 && Y == 0) {
+        cout << "Origem" << endl;
+    } else if (X == 0) {
+        cout << "Eixo Y" << endl;
+    } else if (Y == 0) {
+        cout << "Eixo X" << endl;
+    } else if (X > 0 && Y > 0) {
+        cout << "Q1" << endl;
+    } else if (X < 0 && Y > 0) {
+        cout << "Q2" << endl;
+    } else if (X < 0 && Y < 0) {
+        cout << "Q3" << endl;
+    } else if (X > 0 && Y < 0) {
+        cout << "Q4" << endl;
+    }
+
+    return 0;
+}
 
 
